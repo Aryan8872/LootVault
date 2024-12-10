3f5d600b5ccedd9e2acf0acc381b6ab4338d542b
Aryan8872 <gamersimpact778@gmail.com>
2024-12-10 14:19:34 +0545

    Homescreen: Modified herobar and added animation to it

--- lib/view/homepage_view.dart
+++ lib/view/homepage_view.dart
@@ -1,17 +1,67 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:loot_vault/widgets/horizontal_productcard_list.dart';
 
 class HomePageView extends StatefulWidget {
   const HomePageView({super.key});
 
+  
   @override
   State<HomePageView> createState() => _HomePageViewState();
 }
 
 class _HomePageViewState extends State<HomePageView> {
+  late final PageController pageController;
+
+  late final heroSliderTimer ;
+
+  Timer getTimer(){
+    return Timer.periodic(const Duration(seconds: 3), (timer) {
+      if(pageNo == heroSlider.length-1){
+        pageNo=0;
+      }
+      pageController.animateToPage(pageNo, duration: Duration(seconds: 1), curve: Curves.easeInOutCirc);
+      pageNo++;
+     });
+  }
+
+   final List heroSlider = [
+    "./assets/images/giftcard1.jpg",
+    "./assets/images/giftcard2.jpg",
+    "./assets/images/giftcard3.jpg",
+    "./assets/images/giftcard4.jpg",
+    "./assets/images/giftcard5.jpg",
+    "./assets/images/giftcard6.jpg",
+    "./assets/images/giftcard7.jpg",
+  ];
+
+    int pageNo = 0;
+
+    @override
+  void initState() {
+
+    super.initState();
+    pageController = PageController(
+      initialPage:heroSlider.length~/2,
+      viewportFraction: 0.85
+    );
+    pageNo=heroSlider.length~/2;
+    heroSliderTimer =getTimer();              
+
+  }
+
+
+  @override
+void dispose() {
+  pageController.dispose();
+  super.dispose();
+}
+
   final List popularGameData = [
     "call of duty",
     "gta 5",
@@ -28,7 +78,6 @@ class _HomePageViewState extends State<HomePageView> {
     "./assets/images/giftcard5.jpg",
     "./assets/images/giftcard6.jpg",
     "./assets/images/giftcard7.jpg",
-
   ];
   bool isDark = false;
 
@@ -41,7 +90,17 @@ class _HomePageViewState extends State<HomePageView> {
 
     return Scaffold(
       backgroundColor: const Color.fromRGBO(240, 247, 255, 1),
-   
+      appBar: AppBar(
+        title: Text(
+          "LOOTVAULT",
+          style: GoogleFonts.poppins(
+            fontSize: 25.0,
+            fontWeight: FontWeight.bold,
+            color: Colors.blue,
+          ),
+        ),
+        automaticallyImplyLeading: false,
+      ),
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(12),
@@ -51,7 +110,8 @@ class _HomePageViewState extends State<HomePageView> {
                 builder: (BuildContext context, SearchController controller) {
                   return SearchBar(
                     controller: controller,
-                    backgroundColor: const MaterialStatePropertyAll(Colors.white),
+                    backgroundColor:
+                        const MaterialStatePropertyAll(Colors.white),
                     padding: const MaterialStatePropertyAll<EdgeInsets>(
                       EdgeInsets.symmetric(horizontal: 16.0),
                     ),
@@ -104,38 +164,78 @@ class _HomePageViewState extends State<HomePageView> {
               const SizedBox(
                 height: 30,
               ),
-        
+
               //***********************************************************HERO BAR*************************************************************
-              Container(
-                height: MediaQuery.of(context).orientation == Orientation.portrait
-                    ? MediaQuery.of(context).size.height * 0.23
-                    : null,
-                decoration: const BoxDecoration(),
-                child: ListView.builder(
-                  itemBuilder: (context, index) {
-                    return Container(
-                      padding: EdgeInsets.only(right: 6),
-                      child: ClipRRect(
-                        borderRadius: const BorderRadius.all(Radius.circular(20)),
-                        child: Image.asset(
-                          './assets/images/fb_image.png',
-                          fit: BoxFit.cover,
+              
+
+               SizedBox(
+                height: 200,
+                child: Container(
+                  child: PageView.builder(
+
+                    controller: pageController,
+                    onPageChanged: (index) {
+                      setState(() {
+                                              pageNo = index;
+
+                      });
+                      
+                    },
+                    itemBuilder: (context, index) {
+                    return AnimatedBuilder(
+                      animation: pageController,
+                      builder: (context, child)  {
+                      return child!;
+                    },
+                    child:GestureDetector(
+                      onTap: () {
+                        ScaffoldMessenger.of(context).showSnackBar( SnackBar(content:  Text("page no $index"),backgroundColor: Colors.green,));
+                      },
+                      child: Container(
+                        margin: EdgeInsets.fromLTRB(4, 0, 4, 0),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
                         ),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(12),
+                          child: Image.asset("./assets/images/cod.jpg",fit: BoxFit.cover,)
+                          ),
                       ),
+                    ) ,
                     );
+
                   },
-                  shrinkWrap: true,
-                  itemCount: popularGameData.length,
-                  scrollDirection: Axis.horizontal,
+                  
+                  itemCount: heroSlider.length, 
+                  
+                  ),
                 ),
               ),
-        
+
+              // Buttons for sliderr
+
+              const SizedBox(height: 12,),
+              Row(
+              
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: List.generate(heroSlider.length, (index) => 
+                Container(
+                  margin: const EdgeInsets.only(left: 5),
+                  child:  Icon(Icons.circle,size: 12,
+                  color: pageNo == index? Colors.indigoAccent:Colors.grey,
+                  )
+                  )
+                ),
+                ),
+
               const SizedBox(
                 height: 30,
               ),
-        
+
+
+
               //****************************************BUY GAMES HEADER SECTION*************************************************
-        
+
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
@@ -163,22 +263,24 @@ class _HomePageViewState extends State<HomePageView> {
                   )
                 ],
               ),
-        
+
               SizedBox(
                 height: 10,
               ),
-        
+
               //***************************************BUY GAMES CARDS*****************************************************************
-        
-             
-            HorizontalProductCard(cardData: giftcardData,itemName: "csgo",price: "2000",rating: "4.6",),
-        
+
+              HorizontalProductCard(
+                cardData: giftcardData,
+                itemName: "csgo",
+                price: "2000",
+                rating: "4.6",
+              ),
+
               SizedBox(
                 height: 30,
               ),
-        
-        
-              
+
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
@@ -206,15 +308,18 @@ class _HomePageViewState extends State<HomePageView> {
                   )
                 ],
               ),
-        
-              SizedBox(height: 10,),
-        
-        
-             
-        
+
+              SizedBox(
+                height: 10,
+              ),
+
               // *********************************************************GIFT CARD SECTION**************************************
-            HorizontalProductCard(cardData: giftcardData,itemName: "csgo",price: "2000",),
-        
+              HorizontalProductCard(
+                cardData: giftcardData,
+                itemName: "csgo",
+                price: "2000",
+              ),
+
               //skins and game items
             ]),
           ),
